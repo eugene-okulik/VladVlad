e994c1c78e3208ae55780faec50c7090442723f3
Vladyslav Riaduiev <infovladr@gmail.com>
2024-02-27 18:52:51 +0200

    hw 3 done

---
+++ homework/riaduievv/hw_3/task_1.py
@@ -0,0 +1,10 @@
+a = 5
+b = 5
+
+sum_num = a + b
+subtract_num = a - b
+multip_num = a * b
+
+print(sum_num)
+print(subtract_num)
+print(multip_num)
---
+++ homework/riaduievv/hw_3/task_3.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+
+result_1 = (a + b) / 2
+result_2 = (a * b) ** (1 / 2)
+
+print(result_1)
+print(result_2)
