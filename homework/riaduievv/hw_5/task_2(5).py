7ad50851cef1c513bfa6554d03fc04b12a22528a
Vladyslav Riaduiev <infovladr@gmail.com>
2024-03-03 14:33:36 +0200

    hw 5 done

---
+++ homework/riaduievv/hw_5/task_2(5).py
@@ -0,0 +1,10 @@
+result1 = 'результат операции: 42'
+result2 = 'результат операции: 514'
+result3 = 'результат работы программы: 9'
+
+final_res1 = int(result1[-2:]) + 10
+final_res2 = int(result2[-3:]) + 10
+final_res3 = int(result3[-1]) + 10
+print(final_res1)
+print(final_res2)
+print(final_res3)
