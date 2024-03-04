6c0e19d65a937b437f26ccf458451ddbd73f340f
Vladyslav Riaduiev <infovladr@gmail.com>
2024-03-04 11:57:02 +0200

    hw 5 upd

--- homework/riaduievv/hw_5/task_2(5).py
+++ homework/riaduievv/hw_5/task_2(5).py
@@ -2,9 +2,15 @@ result1 = 'результат операции: 42'
 result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
 
-final_res1 = int(result1[-2:]) + 10
-final_res2 = int(result2[-3:]) + 10
-final_res3 = int(result3[-1]) + 10
-print(final_res1)
-print(final_res2)
-print(final_res3)
+word1 = result1.split()
+last_word1 = word1[-1]
+
+word2 = result2.split()
+last_word2 = word2[-1]
+
+word3 = result3.split()
+last_word3 = word3[-1]
+
+print(int(last_word1) + 10)
+print(int(last_word2) + 10)
+print(int(last_word3) + 10)
