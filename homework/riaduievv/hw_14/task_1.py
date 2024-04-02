44a9f277a7e383c055d1ffb81721217f5c43a9b8
Vladyslav Riaduiev <infovladr@gmail.com>
2024-04-02 23:45:01 +0300

    hw 14 done

--- homework/riaduievv/hw_14/task_1.py
+++ homework/riaduievv/hw_14/task_1.py
@@ -17,14 +17,15 @@ cursor.execute('SELECT * FROM books WHERE taken_by_student_id = 863')
 print(cursor.fetchall())
 
 cursor = db.cursor(dictionary=True)
-cursor.execute('''SELECT students.name, students.second_name, `groups`.title, subjets.title, books.title, lessons.title, marks.value
-FROM students
-JOIN `groups` ON students.group_id = `groups`.id
-JOIN marks ON marks.student_id = students.id
-JOIN lessons ON lessons.id = marks.lesson_id
-JOIN subjets ON subjets.id = lessons.subject_id
-JOIN books ON books.taken_by_student_id = students.id
-WHERE students.id = 863
+cursor.execute('''
+    SELECT students.name, students.second_name, `groups`.title, subjets.title, books.title, lessons.title, marks.value
+    FROM students
+    JOIN `groups` ON students.group_id = `groups`.id
+    JOIN marks ON marks.student_id = students.id
+    JOIN lessons ON lessons.id = marks.lesson_id
+    JOIN subjets ON subjets.id = lessons.subject_id
+    JOIN books ON books.taken_by_student_id = students.id
+    WHERE students.id = 863
 ''')
 print(cursor.fetchall())
 
