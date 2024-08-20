84dcc18de215598f2398bf951b779a7fb6f91401
Vladyslav Riaduiev <infovladr@gmail.com>
2024-08-20 22:16:28 +0300

    hw_16

--- homework/riaduievv/hw_16/task_1_1.py
+++ homework/riaduievv/hw_16/task_1_1.py
@@ -5,26 +5,28 @@ import dotenv
 
 dotenv.load_dotenv(override=True)
 db = mysql.connect(
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    user=os.getenv('DB_USER'),
-    passwd=os.getenv('DB_PASSWD'),
-    database=os.getenv('DB_DATABASE')
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    user=os.getenv("DB_USER"),
+    passwd=os.getenv("DB_PASSWD"),
+    database=os.getenv("DB_DATABASE"),
 )
 cursor = db.cursor(dictionary=True)
 
 base_path = os.path.dirname(__file__)
 home_path = os.path.dirname(os.path.dirname(base_path))
-hw_16_path = os.path.join(home_path, 'eugene_okulik', 'Lesson_16', 'hw_data', 'data.csv')
+hw_16_path = os.path.join(
+    home_path, "eugene_okulik", "Lesson_16", "hw_data", "data.csv"
+)
 
-with open(hw_16_path, newline='') as new_file:
+with open(hw_16_path, newline="") as new_file:
     file_data = csv.DictReader(new_file)
     for row in file_data:
         if isinstance(row, dict):
-            group_title = row.get('group_title')
+            group_title = row.get("group_title")
             if group_title:
                 query = """
-                SELECT students.name, students.second_name, 
+                SELECT students.name, students.second_name,
                 `groups`.title, subjets.title, books.title, lessons.title, marks.value
                 FROM students
                 JOIN `groups` ON students.group_id = `groups`.id
